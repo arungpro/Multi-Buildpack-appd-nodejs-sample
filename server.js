6f12ffda11da7573b6e6a4b14d345dbe782aa017
arungpro <arungangadharaiah@appdgmail.com>
2020-08-05 15:28:14 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,10 @@
+require('appdynamics').profile({})
+var express = require( 'express')
+var app = express()
+
+
+app.get( '/', function ( req, res) {
+	res.send("Hello world")
+})
+
+app.listen( process.env.PORT || 4000)
